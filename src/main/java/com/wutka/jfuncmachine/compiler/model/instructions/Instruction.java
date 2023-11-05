69c3817b0229b84ec3c7f77ff76a4f070e6bd7cf
Mark Wutka <mark.wutka@vanderbilt.edu>
2023-11-05 16:15:44 -0500

    Removed LambdaFunction and Instruction

--- src/main/java/com/wutka/jfuncmachine/compiler/model/instructions/Instruction.java
+++
@@ -1,8 +0,0 @@
-package com.wutka.jfuncmachine.compiler.model.instructions;
-
-import com.wutka.jfuncmachine.compiler.model.types.Type;
-
-public abstract class Instruction {
-    public abstract Type[] getArgumentTypes();
-    public abstract Type getResultType();
-}
