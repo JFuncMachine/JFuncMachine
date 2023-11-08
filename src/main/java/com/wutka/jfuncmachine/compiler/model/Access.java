baf58191ca8ce29b377d534662adedc2093754b3
Mark Wutka <mark@wutka.com>
2023-11-07 21:10:41 -0500

    Added some access types

--- src/main/java/com/wutka/jfuncmachine/compiler/model/Access.java
+++ src/main/java/com/wutka/jfuncmachine/compiler/model/Access.java
@@ -3,8 +3,11 @@ package com.wutka.jfuncmachine.compiler.model;
 import org.objectweb.asm.Opcodes;
 
 public class Access {
-    public static final int PUBLIC = Opcodes.ACC_PUBLIC;
-    public static final int PROTECTED = Opcodes.ACC_PROTECTED;
+    public static final int ABSTRACT = Opcodes.ACC_ABSTRACT;
+    public static final int FINAL = Opcodes.ACC_FINAL;
+    public static final int INTERFACE = Opcodes.ACC_INTERFACE;
     public static final int PRIVATE = Opcodes.ACC_PRIVATE;
+    public static final int PROTECTED = Opcodes.ACC_PROTECTED;
+    public static final int PUBLIC = Opcodes.ACC_PUBLIC;
     public static final int STATIC = Opcodes.ACC_STATIC;
 }
